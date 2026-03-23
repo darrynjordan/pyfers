7bd7ad3f323f339414558646f5050e3386240eeb
Darryn Jordan <darrynjordan@icloud.com>
2026-03-23 13:24:29 +0300

    Added component classes.

--- pyfers/__init__.py
+++ pyfers/__init__.py
@@ -2,7 +2,376 @@ import os
 import h5py
 import numpy as np
 import subprocess as sbp
+import scipy.constants as constants
 from lxml import etree as ET
+
+def wavelength_to_frequency(wavelength):
+    return constants.c/wavelength
+
+def frequency_to_wavelength(frequency):
+    return constants.c/frequency
+
+def time_to_range(time):
+    return constants.c*time/2
+
+def range_to_time(range):
+    return 2*range/constants.c
+
+def linear_to_dB(linear):
+    return 10*np.log10(linear)
+
+def dB_to_linear(decibels):
+    return pow(10, decibels/10)
+
+def achirp(period, sample_rate, bandwidth, init_freq=0, tau=0, phi=0):
+    '''
+    Generate an analytic baseband chirp.
+    '''
+    ns_chirp = int(np.ceil(period*sample_rate))
+    t_chirp = np.linspace(-period/2, period/2, ns_chirp, endpoint=False)
+    return np.exp(1.j*(np.pi*bandwidth/(2*max(t_chirp))*pow((t_chirp - tau), 2) + 2*np.pi*init_freq*(t_chirp - tau) + phi))
+
+class Antenna:
+    def __init__(self, name:str, type:str, gain:float, efficiency:float=1, az_beta=None, el_beta=None, az_gamma=None, el_gamma=None, az_factor=None, el_factor=None):
+        """
+        Parameters
+        ----------
+            name : str
+                Unique antenna name.
+            type : str
+                Type of antenna. ['sinc', 'gaussian']
+            gain : float
+                Antenna gain (dBi).
+            efficiency : float
+                Antenna efficiency.
+        """
+        self._name = name
+        self._type = type.lower()
+        self._gain = dB_to_linear(gain)
+        self._efficiency = efficiency
+        self._theta = np.linspace(-np.pi, np.pi, 1001, endpoint=False)
+        self._az_pattern = None
+        self._el_pattern = None
+
+        if self._type == 'sinc':
+            if (az_beta is None) or (el_beta is None) or (az_gamma is None) or (el_gamma is None):
+                print("ERROR: All beta and gamma parameters are required for sinc antenna.")
+            self._az_beta = az_beta
+            self._el_beta = el_beta
+            self._az_gamma = az_gamma
+            self._el_gamma = el_gamma
+            self._az_pattern = self._gain * pow((np.sin(self._az_beta*self._theta)/(self._az_beta*self._theta)), self._az_gamma)
+            self._el_pattern = self._gain * pow((np.sin(self._el_beta*self._theta)/(self._el_beta*self._theta)), self._el_gamma)
+        elif self._type == 'gaussian':
+            if (az_factor is None) or (el_factor is None):
+                print("ERROR: All factor parameters are required for gaussian antenna.")
+            self._az_factor = az_factor
+            self._el_factor = el_factor
+            self._az_pattern = self._gain * np.exp(-1*pow(self._theta, 2)*self._az_factor)
+            self._el_pattern = self._gain * np.exp(-1*pow(self._theta, 2)*self._el_factor)
+        else:
+            print("ERROR: Unsupported antenna type.")
+
+    @property
+    def theta(self):
+        return self._theta
+
+    @property
+    def az_pattern(self):
+        return self._az_pattern
+
+    @property
+    def el_pattern(self):
+        return self._el_pattern
+
+
+class Waveform:
+    def __init__(self, f_carrier:float, type:str, power:float, f_sample:float, bandwidth:float=None, t_pulse:float=None):
+        """
+        Parameters
+        ----------
+            f_carrier : float
+                Carrier frequency (Hz).
+            type : str
+                Type of waveform. ['pulse', 'cw']
+            power : float
+                Constant power of the pulse (W).
+            f_sample : float
+                Rate at which the waveform is sampled (Hz).
+            bandwidth : float
+                Bandwidth of the waveform (Hz).
+            t_pulse : float
+                Length of the pulse (s).
+        """
+        self._f_carrier = f_carrier
+        self._type = type
+        self._power = power
+        self._f_sample = f_sample
+        self._bandwidth = bandwidth
+        self._t_pulse = t_pulse
+        self._samples = None
+        self._n_samples = None
+
+        if (self._f_sample < self._bandwidth):
+            print("WARNING: Sample rate is insufficient. Nyquist is violated.")
+
+        if type == 'pulse':
+            self._samples = achirp(period=self._t_pulse, sample_rate=self._f_sample, bandwidth=self._bandwidth, tau=0)
+
+    @property
+    def f_carrier(self):
+        return self._f_carrier
+
+    @property
+    def wavelength(self):
+        return frequency_to_wavelength(self._f_carrier)
+
+    @property
+    def power(self):
+        return self._power
+
+    @property
+    def bandwidth(self):
+        return self._bandwidth
+
+    @property
+    def samples(self):
+        return self._samples
+
+    @property
+    def n_samples(self):
+        return len(self._samples)
+
+class Clock:
+    def __init__(self, name:str, frequency:float, f_offset:float=0, random_f_offset:float=0, p_offset:float=0, random_p_offset:float=0):
+        """
+        Parameters
+        ----------
+            name : str
+                Unique name of the clock.
+            frequency : float
+                Frequency of the clock (Hz).
+            f_offset : float
+                Constant frequency offset (Hz).
+            random_f_offset : float
+                Standard deviation of random frequency offset (Hz).
+            p_offset : float
+                Constant phase offset (rad).
+            random_p_offset : float
+                Standard deviation of random phase offset (rad).
+        """
+        self._name = name
+        self._frequency = frequency
+        self._f_offset = f_offset
+        self._p_offset = p_offset
+        self._random_f_offset = random_f_offset
+        self._random_p_offset = random_p_offset
+
+class Transmitter:
+    def __init__(self, antenna:Antenna, waveform:Waveform, clock:Clock, f_prf:float):
+        """
+        Parameters
+        ----------
+            antenna : Antenna
+                Antenna used for transmission.
+            waveform : Waveform
+                Waveform to transmit.
+            clock : Clock
+                Timing source for the transmitter.
+            f_prf : float
+                Pulse repetition frequency (Hz).
+        """
+        self._antenna = antenna
+        self._waveform = waveform
+        self._clock = clock
+        self._f_prf = f_prf
+
+    @property
+    def f_prf(self):
+        return self._f_prf
+
+    @property
+    def t_pri(self):
+        return 1/self._f_prf
+
+    @property
+    def duty_cycle(self):
+        return self._waveform._t_pulse / self.t_pri
+
+    @property
+    def p_average(self):
+        return self._waveform.power * self._duty_cycle
+
+class Receiver:
+    def __init__(self, name:str, antenna:Antenna, clock:Clock, f_prf:float, range_gate:float=None, noise_temp:float=290):
+        """
+        Parameters
+        ----------
+            name : str
+                Unique name for the receiver.
+            antenna : Antenna
+                Antenna used for transmission.
+            clock : Clock
+                Timing source for the receiver.
+            f_prf : float
+                The receiver pulse repetition frequency (Hz).
+            range_gate : float
+                The extent of range to digitise (m). Starts from zero metres. If None, the maximum range gate is used.
+            noise_temp : float
+                The noise temperature of the receiver (K).
+        """
+        self._name = name
+        self._antenna = antenna
+        self._f_prf = f_prf
+        self._range_gate = range_gate
+        self._noise_temp = noise_temp
+
+        if self._range_gate is None:
+            self._range_gate = time_to_range(1/self._f_prf)
+        else:
+            if (self._range_gate < 0) or (self._range_gate > time_to_range(1/self._f_prf)):
+                print("ERROR: Invalid range gate.")
+
+    @property
+    def noise_temp(self):
+        return self._noise_temp
+
+    @property
+    def noise_density(self):
+        """
+        Calculate the noise density (W/Hz).
+        """
+        return constants.Boltzmann * self._noise_temp
+
+    # TODO add method for calculating ns_range_gate
+
+class Platform:
+    """
+    Along-Track (m):    X-Axis
+    Cross-Track (m):    Y-Axis
+    Altitude (m):       Z-Axis
+    Azimuth (deg):      X-Y Plane
+    Elevation (deg):    X-Z Plane
+    """
+    def __init__(self, d:float, fs:float, altitude:float, velocity:float, depression:float=0, squint:float=0):
+        """
+        Parameters
+        ----------
+            d : float
+                Duration of platform motion (s).
+            fs : float
+                Sample rate of platform motion (Hz).
+            altitude : float
+                Mean altitude of the platform (m).
+            velocity : float
+                Mean velocity of the platform (m/s).
+            depression : float
+                Mean depression angle of the sensor (deg).
+            squint : float
+                Mean squint angle of the platform (deg).
+        """
+        self.duration = d
+        self.fs = fs
+        self.altitude = altitude
+        self.velocity = velocity
+        self.depression = depression
+        self.squint = squint
+
+        # sampled time axis
+        self._t = np.linspace(0, self.duration, self.n_samples, endpoint=False)
+
+        # arrays that hold the ideal motion
+        self._x = self.velocity * self._t
+        self._y = np.zeros(self.n_samples)
+        self._z = self.altitude * np.ones(self.n_samples)
+        self._az = self.squint * np.ones(self.n_samples)
+        self._el = self.depression * np.ones(self.n_samples)
+
+        # arrays that hold the deviations from ideal motion
+        self._x_noise = 0
+        self._y_noise = 0
+        self._z_noise = 0
+        self._az_noise = 0
+        self._el_noise = 0
+
+    def add_sinusoidal_noise(self, axis:str, amplitude:float, frequency:float, phase:float):
+        """
+        Parameters
+        ----------
+            axis : str
+                Axis to which the sinusoidal noise is applied.
+                ['x', 'y', 'z', 'az', 'el']
+            amplitude : float
+                Amplitude of the sinusoid (m).
+            frequency : float
+                Frequency of the sinusoid (Hz). Must be less than fs/2.
+            phase : float
+                Phase of the sinusoid (rad).
+        """
+        if frequency > self.fs/2:
+            print("Error: The frequency of sinusoidal motion noise violates Nyquist. No noise applied.")
+            return
+
+        noise = amplitude * np.sin(2*np.pi*frequency*self._t + phase)
+
+        if axis.lower() == 'x':
+            self._x_noise += noise
+        elif axis.lower() == 'y':
+            self._y_noise += noise
+        elif axis.lower() == 'z':
+            self._z_noise += noise
+        elif axis.lower() == 'az':
+            self._az_noise += noise
+        elif axis.lower() == 'el':
+            self._el_noise += noise
+        else:
+            print("Error: Unknown axis, use ['x', 'y', 'z', 'az', 'el'].")
+
+    @property
+    def n_samples(self):
+        return int(np.floor(self.duration * self.fs))
+
+    @property
+    def t(self):
+        """
+        Sampled time axis.
+        """
+        return self._t
+
+    @property
+    def x(self):
+        """
+        Sampled motion in x-axis (along-track).
+        """
+        return self._x + self._x_noise
+
+    @property
+    def y(self):
+        """
+        Sampled motion in y-axis (cross-track).
+        """
+        return self._y + self._y_noise
+
+    @property
+    def z(self):
+        """
+        Sampled motion in z-axis (altitude).
+        """
+        return self._z + self._z_noise
+
+    @property
+    def az(self):
+        """
+        Sampled motion in azimuth (squint).
+        """
+        return self._az + self._az_noise
+
+    @property
+    def el(self):
+        """
+        Sampled motion in elevation (depression).
+        """
+        return self._el + self._el_noise
 
 def write_hdf5(dataset, filename):
     '''
