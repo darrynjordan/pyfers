bb10b4bd85b8b2e8f10ce405dcf8e5fdedf44a63
Darryn Jordan <darrynjordan@icloud.com>
2026-03-27 08:40:08 +0300

    Updated description and URL.

--- setup.py
+++ setup.py
@@ -5,16 +5,16 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="pyfers",
-    version='2.0.1',
+    version='2.0.2',
     author="Darryn Anton Jordan",
     author_email="<darrynjordan@icloud.com>",
-    description='XML generator for FERS',
+    description='Generates XML Descriptors for FERS',
     long_description=long_description,
     long_description_content_type="text/markdown",
     packages=find_packages(),
     install_requires=['numpy', 'h5py', 'lxml', 'scipy'],
     keywords=['radar', 'simulation'],
-    url="https://github.com/darrynjordan/FERS",
+    url="https://github.com/darrynjordan/pyfers",
     classifiers=[
         "Development Status :: 1 - Planning",
         "Intended Audience :: Developers",
